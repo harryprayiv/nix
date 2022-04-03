401d0690325bc081ecf2f7c8b0a266c81ffacf6d
harryprayiv <harryprayiv@gmail.com>
2022-04-03 01:36:58 -0400

    updated plutusVM hardware config

--- system/machine/plutusVM/hardware-configuration.nix
+++ system/machine/plutusVM/hardware-configuration.nix
@@ -2,24 +2,18 @@
 
 {
     fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4508c421-4cb6-4b60-bcac-3915c18abdad";
+    { device = "/dev/disk/by-uuid/86be1ae9-1579-4445-be92-b881eed0c647";
       fsType = "ext4";
     };
 
 fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B307-7565";
+    { device = "/dev/disk/by-uuid/FE8C-EB72";
       fsType = "vfat";
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/23d6c9ef-5721-4e99-81e4-40d68e32e0a8"; }
+    [ { device = "/dev/disk/by-uuid/5348e4f1-888a-439f-9bbc-e3ddd271087c"; }
     ];
-
-fileSystems."/home/plutusVM/NAS/Programming" =
-  { device = "192.168.1.212:/volume2/Programming";
-    options = [ "x-systemd.automount" "noauto" ];
-    fsType = "nfs";
-  };
 
 fileSystems."/home/plutusVM/NAS/plutus" =
   { device = "192.168.1.212:/volume2/homes/plutus";
