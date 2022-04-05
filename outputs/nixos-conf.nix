080207724caa74c2dbff64cb92ed6f66ad136d1b
harryprayiv <harryprayiv@gmail.com>
2022-04-05 07:16:50 -0400

    limiting options of installer

--- outputs/nixos-conf.nix
+++ outputs/nixos-conf.nix
@@ -10,41 +10,4 @@
       ../system/configuration.nix
     ];
   };
-
-  onyx = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../system/machine/onyx
-      ../system/configuration.nix
-    ];
-  };
-
-  feather = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../system/machine/feather
-      ../system/configuration.nix
-    ];
-  };
-
-  dell-xps = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../system/machine/dell-xps
-      ../system/configuration.nix
-    ];
-  };
-
-  tongfang-amd = lib.nixosSystem {
-    inherit system;
-    specialArgs = { inherit inputs; };
-    modules = [
-      ../system/machine/tongfang-amd
-      ../system/configuration.nix
-    ];
-  };
-
 }
