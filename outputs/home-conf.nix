5068dc218da2dea20b769917d7986848a4832848
harryprayiv <harryprayiv@gmail.com>
2022-04-13 13:35:39 -0400

    converted to plutusVM

--- outputs/home-conf.nix
+++ outputs/home-conf.nix
@@ -1,7 +1,7 @@
 { system, nixpkgs, nurpkgs, home-manager, tex2nix, ... }:
 
 let
-  username = "gvolpe";
+  username = "plutusVM";
   homeDirectory = "/home/${username}";
   configHome = "${homeDirectory}/.config";
 
@@ -44,6 +44,6 @@ let
   };
 in
 {
-  gvolpe-edp = mkHome edpConf;
-  gvolpe-hdmi = mkHome hdmiConf;
+  plutusVM-edp = mkHome edpConf;
+  plutusVM-hdmi = mkHome hdmiConf;
 }
