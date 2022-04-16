6981a7ea40c878e542dd103fc8f7d341d4b301e8
harryprayiv <harryprayiv@gmail.com>
2022-04-16 19:07:07 -0400

    cosmetic package stuff

--- home/home.nix
+++ home/home.nix
@@ -69,45 +69,45 @@ let
     binutils-unwrapped
     #__________new_____________
       ## Text Editors ##
-  alacritty vscode atom
+    alacritty vscode atom
 
-  ## Web Browsers ##
-  brave
-  ## Chat Apps ##
-  discord #telegram-desktop signal-desktop
+    ## Web Browsers ##
+    brave
+    ## Chat Apps ##
+    discord #telegram-desktop signal-desktop
 
-  ## Github ##
-  mr gitAndTools.gitFull jq
+    ## Github ##
+    mr gitAndTools.gitFull jq
 
-  ## Basic Hacker Stuff ##
-  gparted nfs-utils sshfs mtr sysstat
-  wget coreutils binutils curl
-  zip unzip killall syncthing-cli
-  htop
-  #vimHugeX 
-  sudo iptables nmap tcpdump rxvt_unicode #xscreensaver
+    ## Basic Hacker Stuff ##
+    gparted nfs-utils sshfs mtr sysstat
+    wget coreutils binutils curl
+    zip unzip killall syncthing-cli
+    htop
+    #vimHugeX 
+    sudo iptables nmap tcpdump rxvt_unicode #xscreensaver
 
-  ## Coding ##
-  gcc zlib bc man-pages
-  zulu #OpenJDK 11
+    ## Coding ##
+    gcc zlib bc man-pages
+    zulu #OpenJDK 11
 
-  ## Piracy ##
-  openvpn 
-  transmission 
-  vlc 
-  ffmpeg 
-  mkvtoolnix
+    ## Piracy ##
+    openvpn 
+    transmission 
+    vlc 
+    ffmpeg 
+    mkvtoolnix
 
-  ## Nix Package for nix-shell etc ##
-  binutils 
-  cabal2nix
+    ## Nix Package for nix-shell etc ##
+    binutils 
+    cabal2nix
   
-  ## X11 Packages ##
-  trayer
+    ## X11 Packages ##
+    trayer
 
-  ## Haskell packages and Xmonad Stuff ##
-  cabal-install 
-  ghc
+    ## Haskell packages and Xmonad Stuff ##
+    cabal-install 
+    ghc
   ];
 
   gitPkgs = with pkgs.gitAndTools; [
