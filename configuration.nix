60df1cdd7ad7bf8434669c6deba1b163302b5fc0
Harry Pray IV <harryprayiv@gmail.com>
2022-07-04 21:19:36 -0400

    Update configuration.nix
    Signed-off-by: Harry Pray IV <harryprayiv@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -103,24 +103,35 @@
   };
 
   environment.systemPackages = with pkgs; [ 
-  ## Text Editors ##
-  alacritty
+  alacritty vscode
+  
   ## Github ##
   mr gitAndTools.gitFull jq
+
   ## Basic Hacker Stuff ##
-  gparted nfs-utils sshfs mtr sysstat wget coreutils 
-  binutils curl zip unzip killall syncthing-cli asciidoctor 
-  appimage-run bind cpulimit file fzf iperf jpegoptim 
-  libwebp lm_sensors lsof man mtr nmap ntfs3g openssl 
-  pciutils pdftk pandoc pwgen rclone rsync traceroute 
-  tree unetbootin unrar unzip usbutils vim which 
-  yamllint sudo iptables nmap tcpdump rxvt_unicode 
-  filelight gparted parted libreoffice-fresh p7zip remmina 
-  zim byobu ncurses oh-my-zsh screen sshpass tmux zsh
+  discord tdesktop
+  freecad kicad
+  gparted nfs-utils sshfs mtr sysstat
+  wget coreutils binutils curl
+  zip unzip killall syncthing-cli
+  asciidoctor appimage-run bind cpulimit file fzf iperf jpegoptim libwebp lm_sensors
+  lsof man mtr nmap ntfs3g openssl pciutils pdftk pandoc pwgen rclone rsync traceroute
+  tree unetbootin unrar unzip usbutils vim which yamllint
+  sudo iptables nmap tcpdump rxvt_unicode 
+  filelight gparted libreoffice-fresh p7zip remmina zim
+  byobu ncurses oh-my-zsh screen sshpass tmux zsh
+  #added to enable cardano node tools: 
+  gmpxx openssl.dev libffi systemd zlib procps sqlite
+  
+  ##For Cardano Guild apps on Debian: build-essential pkg-config libffi-dev libgmp-dev libssl-de>
+  ## for CentOS pkgconfig libffi-devel gmp-devel openssl-devel ncurses-libs ncurses-compat-libs >
+
   ## Nix Package for nix-shell etc ##
   binutils 
   cabal2nix
   nix
+  docker
+  
   gnome3.gnome-tweaks
   ];
 
